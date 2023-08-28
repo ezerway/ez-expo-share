9c93e394738d7b3dfe48bca9da28cbc3ec967c0b
ezerway <54234094+ezerway@users.noreply.github.com>
2023-08-28 17:14:58 +0700

    chore: suport android os

--- android/src/main/java/expo/modules/ezexposhare/EzExpoShareModule.kt
+++ android/src/main/java/expo/modules/ezexposhare/EzExpoShareModule.kt
@@ -2,15 +2,12 @@ package expo.modules.ezexposhare
 
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
-import android.content.BroadcastReceiver
-import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
-import android.os.Build
 import android.os.Bundle
-import expo.modules.kotlin.exception.Exceptions
-import expo.modules.kotlin.types.Enumerable
-import java.lang.ref.WeakReference
+import android.net.Uri
+import android.os.Build
+import android.os.Build.VERSION
+import android.os.Parcelable
 
 class EzExpoShareModule : Module() {
   // Each module class must implement the definition function. The definition consists of components
@@ -22,63 +19,21 @@ class EzExpoShareModule : Module() {
     // The module will be accessible from `requireNativeModule('EzExpoShare')` in JavaScript.
     Name("EzExpoShare")
 
-    Constants("isSupported" to true)
-
     // Defines event names that the module can send to JavaScript.
     Events("onChange")
 
-    OnCreate {
-      registerBroadcastReceivers(context)
-    }
-
-    OnDestroy {
-      unregisterBroadcastReceivers(context)
-    }
-  }
-
-  private val context: Context
-    get() = appContext.reactContext ?: throw Exceptions.ReactContextLost()
-
-  private val broadcastReceivers = mutableListOf<BroadcastReceiver>()
-
-  private inline fun accessBroadcastReceivers(block: MutableList<BroadcastReceiver>.() -> Unit) {
-    synchronized(broadcastReceivers) {
-      block.invoke(broadcastReceivers)
-    }
-  }
-
-  private fun unregisterBroadcastReceivers(context: Context) {
-    accessBroadcastReceivers {
-      forEach {
-        context.unregisterReceiver(it)
+    OnNewIntent { intent ->
+      val uri = if (VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        intent.getParcelableExtra(Intent.EXTRA_STREAM, Parcelable::class.java) as? Uri
+      } else {
+        intent.getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as? Uri
       }
-      clear()
-    }
-  }
 
-  private fun registerBroadcastReceivers(context: Context) {
-    accessBroadcastReceivers {
-      if (isNotEmpty()) {
-        return
-      }
-    }
-
-    val weakModule = WeakReference(this@EzExpoShareModule)
-    val emitEvent = { name: String, body: Bundle ->
-      try {
-        // It may thrown, because RN event emitter may not be available
-        // we can just ignore those cases
-        weakModule.get()?.sendEvent(name, body)
-      } catch (_: Throwable) {
-      }
-      Unit
-    }
-    val receiveImageReceiver = EzExpoShareReceiveImageReceiver(emitEvent)
-
-    context.registerReceiver(receiveImageReceiver, IntentFilter("android.intent.action.SEND"))
-
-    accessBroadcastReceivers {
-      add(receiveImageReceiver)
+      sendEvent("onChange",
+        Bundle().apply {
+          putString("image_uri", uri.toString())
+        }
+      )
     }
   }
 }
